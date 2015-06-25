970bf0e63017867a0b96daf942acac12e71f4c70
tourenathan <teyou21@gmail.com>
2015-06-25 22:09:13 +0100

    Created the model, configure the url.py file, and created an index fucntion in the view.py

--- SimpleCrud/settings.py
+++ SimpleCrud/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crud',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,7 +59,7 @@ WSGI_APPLICATION = 'SimpleCrud.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'crud.sqlite3'),
     }
 }
 
